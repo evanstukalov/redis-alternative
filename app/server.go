0550c420cd92b44eead66a0a6c4383792ad9949a
evanstukalov <evanstukalov@gmail.com>
2024-08-02 18:55:04 +0300

    feat: implement the ECHO command

--- app/server.go
+++ app/server.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"net"
 	"os"
+	"strconv"
+	"strings"
 )
+
+const DELIM = "\r\n"
 
 func main() {
 	fmt.Println("Logs from your program will appear here!")
@@ -28,14 +34,84 @@ func main() {
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
 
-	buf := make([]byte, 1024)
-
 	for {
-		_, err := conn.Read(buf)
+		r := bufio.NewReader(conn)
+
+		args, err := unpackInput(r)
 		if err != nil {
-			fmt.Println("Error reading from connection", err.Error())
 			break
 		}
-		conn.Write([]byte("+PONG\r\n"))
+
+		if len(args) == 0 {
+			break
+		}
+
+		switch strings.ToUpper(args[0]) {
+		case "PING":
+			conn.Write([]byte("+PONG\r\n"))
+		case "ECHO":
+			msg := args[1]
+			conn.Write([]byte(fmt.Sprintf("$%d\r\n%s\r\n", len(msg), msg)))
+		default:
+			conn.Write([]byte("-Error\r\n"))
+		}
+
 	}
+}
+
+func parseLen(data []byte) (int, error) {
+	n := strings.Trim(string(data), "\r\n")
+	return strconv.Atoi(n)
+}
+
+func unpackInput(r *bufio.Reader) ([]string, error) {
+	args := make([]string, 0, 64)
+
+	firstLine, err := r.ReadBytes('\n')
+	if err != nil {
+		return nil, err
+	}
+
+	var commandLen int
+
+	if firstLine[0] == '*' {
+		commandLen, err = parseLen(firstLine[1:])
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	for i := 0; i < commandLen; i++ {
+
+		line, err := r.ReadBytes('\n')
+		if err != nil {
+			return nil, err
+		}
+
+		var argLen int
+
+		if line[0] == '$' {
+			argLen, err = parseLen(line[1:])
+			if err != nil {
+				fmt.Println("Error: ", err)
+				return nil, err
+			}
+		}
+
+		if argLen == 0 {
+			continue
+		}
+
+		buf := make([]byte, argLen+len(DELIM))
+
+		if _, err = io.ReadFull(r, buf); err != nil {
+			fmt.Println("Error: ", err)
+			return nil, err
+		}
+
+		arg := strings.Trim(string(buf), DELIM)
+		args = append(args, arg)
+	}
+
+	return args, nil
 }
